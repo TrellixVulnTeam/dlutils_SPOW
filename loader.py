3bd731c8f16e5c83b3763a371493566cbc1feee3
Gorkem <Algan>
2019-07-02 15:17:06 +0300

    something

--- loader.py
+++ loader.py
@@ -1,13 +1,10 @@
-import gzip
-import pickle
-import sys
+import keras
+from keras.datasets import mnist
 
-def load_mnist(data_folder):
-    f = gzip.open(data_folder + 'mnist.pkl.gz', 'rb')
-    if sys.version_info < (3,):
-        data = pickle.load(f)
-    else:
-        data = pickle.load(f, encoding='bytes')
-    f.close()
-    (x_train, y_train), (x_test, y_test) = data
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(60000, 784).astype('float32') / 255
+    x_test = x_test.reshape(10000, 784).astype('float32') / 255
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
     return x_train, y_train, x_test, y_test
--- visualizer.py
+++ visualizer.py
@@ -12,6 +12,24 @@ def plot_dotted(ax, x_data, y_data, label="dotted data"):
 def plot_lined(ax, x_data, y_data, label="lined data"):
     return ax.plot(x_data, y_data, label=label)
 
+def plot_accs(train_acc, val_acc):
+    plt.plot(train_acc)
+    plt.plot(val_acc)
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def plot_losses(train_loss, val_loss):
+    plt.plot(train_loss)
+    plt.plot(val_loss)
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
 def add2gif(fig, images):
     # Used to return the plot as an image rray
     fig.canvas.draw();       # draw the canvas, cache the renderer
